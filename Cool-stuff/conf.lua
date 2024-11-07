7634e430d76a5de8a64693f473c5d41117dfc2f9
xxEllie <elliebunnycute@gmail.com>
2024-11-07 12:43:15 -0700

    holy shit another bagel

--- Cool-stuff/conf.lua
+++ Cool-stuff/conf.lua
@@ -1,4 +1,6 @@
 function love.conf(t)
-    t.window.width = 100
-    t.window.height = 500
+    t.window.title = "bagel";
+    t.window.width = 360
+    t.window.height = 360
+    t.window.borderless = true
 end
--- Cool-stuff/main.lua
+++ Cool-stuff/main.lua
@@ -1,13 +1,21 @@
 local ffi = require("ffi")
-
+local bit = require("bit")
 ffi.cdef[[
-    void* GetActiveWindow();
+    void* GetActiveWindow();    
     int SetWindowPos(void* hWnd, int hWndInsertAfter, int X, int Y, int cx, int cy, unsigned int uFlags);
+    int SetWindowLongA(void* hWnd, int nIndex, long dwNewLong);
+    long GetWindowLongA(void* hWnd, int nIndex);
+    bool SetLayeredWindowAttributes(void* hwnd, int crKey, unsigned char bAlpha, int dwFlags);
 ]]
-
+local bagel
 local hwnd = ffi.C.GetActiveWindow()
 local SWP_NOSIZE = 0x0001
 local HWND_TOP = 0
+local GWL_EXSTYLE = -20
+local WS_EX_LAYERED = 0x80000
+local WS_EX_TRANSPARENT = 0x20
+local LWA_COLORKEY = 0x1
+
 local function setWindowPosition(x, y)
     ffi.C.SetWindowPos(hwnd, HWND_TOP, x, y, 0, 0, SWP_NOSIZE)
 end
@@ -18,6 +26,13 @@ local velocity = 0
 local windowX, windowY = 100, 100
 local floorY = 600
 local moveSpeed = 400
+function love.load()
+    bagel = love.graphics.newImage("/assets/bagel.png")
+
+    local exStyle = ffi.C.GetWindowLongA(hwnd, GWL_EXSTYLE)
+    ffi.C.SetWindowLongA(hwnd, GWL_EXSTYLE, bit.bor(exStyle, WS_EX_LAYERED, WS_EX_TRANSPARENT))
+    ffi.C.SetLayeredWindowAttributes(hwnd, 0x000000, 0, LWA_COLORKEY)
+end
 
 function love.update(dt)
     velocity = velocity + gravity * dt
@@ -39,3 +54,6 @@ function love.update(dt)
     setWindowPosition(windowX, windowY)
 end
 
+function love.draw()
+    love.graphics.draw(bagel, 0, 0)
+end
